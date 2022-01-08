fdb99c6d23cf5232f45a5b68b5a4293ab48df29a
abg3000 <abg3000@naver.com>
2022-01-09 02:52:46 +0900

    netlify deploy finish

--- src/module/useeffects.js
+++ src/module/useeffects.js
@@ -1,18 +1,15 @@
 export const translateSlide = (data, slideRef, currentBanner) => {
   let totalWidth;
-  let check;
 
   if (window.innerWidth >= 1200) {
     totalWidth = 1084 * data.length;
-    check = 1084;
     slideRef.current.style.transform = `translateX(-${
       (totalWidth / data.length) * currentBanner -
       (window.innerWidth - 1060) / 2 +
-      60
+      50
     }px)`;
   } else {
     totalWidth = (window.innerWidth - 76) * data.length;
-    check = window.innerWidth - 100;
     slideRef.current.style.transform = `translateX(-${
       (totalWidth / data.length) * currentBanner + 2
     }px)`;
